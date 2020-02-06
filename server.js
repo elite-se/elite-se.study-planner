a1261e18dfa570e44d07d10d2fd2971563f4ffdc
FabianWildgrube <fabian.wildgrube@gmx.de>
2020-02-06 19:47:00 +0100

    added configuration and minimal express server for heroku deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/se-planner"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/se-planner/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
