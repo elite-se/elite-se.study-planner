4a948c93f06289f622fca6dedecc4cd3b2c3ef26
FabianWildgrube <fabian.wildgrube@gmx.de>
2020-02-06 19:57:06 +0100

    serve studiengangsdata through backend

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/se-planner"));
+app.use("/", express.static(__dirname + "/dist/se-planner"));
+app.use("/data", express.static(__dirname + "/data"));
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/se-planner/index.html"));
